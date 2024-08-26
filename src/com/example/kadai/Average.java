ce910e748cbc01f89be3b034b77b0621365bde82
kazuo <kazuomizusawa1655@gmail.com>
2024-08-26 10:07:24 +0900

    first commit

---
+++ src/com/example/kadai/Average.java
@@ -0,0 +1,12 @@
+package com.example.kadai;
+
+public class Average {
+
+    public static void main(String[] args) {
+        double x = 24.8;
+        double y = 16.4;
+        double average = (x + y) / 2;
+        System.out.println("xとyの平均は" + average + "です。");
+    }
+
+}
